c99d11961163d3a38a322b059ac2eef4d9b0ce3c
Jamesson Santos <jamesson1233@gmail.com>
2024-12-12 05:58:42 -0300

    feat: add ConfigMap for names and update deployment to use it

--- server.go
+++ server.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -18,4 +21,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s! v3 from %s", name, envir)
 
 	w.Write([]byte("Hello, World! v3"))
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/files/file.txt")
+
+	if err != nil {
+		log.Fatalf("failed reading data from file: %s", err)
+	}
+
+	fmt.Fprintf(w, "Data from file: %s", string(data))
 }
